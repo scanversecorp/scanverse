6aa996c970149742149cebf19bdfd591ddcac1c5
scanversecorp <scanversecorp@gmail.com>
2026-08-09 12:25:51 +0530

    ScanV v4: index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+
+/* Remove the pre-React loader spinner */
 const loader = document.getElementById('root-loader');
 if (loader) setTimeout(() => loader.remove(), 100);
